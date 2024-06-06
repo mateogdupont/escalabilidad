2980ffc27ff5b6c05a9d28d0ceadd7914f75908f
ElianaHarriet <eharriet@fi.uba.ar>
2024-06-05 21:22:55 -0300

    feat: Add LogManager class for logging counted data and data sent

---
+++ src/counter/log_manager.py
@@ -0,0 +1,29 @@
+from typing import Tuple
+from utils.basic_log_manager import *
+from utils.structs.data_fragment import DataFragment
+import pickle
+
+RECEIVED_ID = "RECEIVED_ID"             # <client_id> <query_id> <df_id>
+COUNTED_DATA = "COUNTED_DATA"           # <client_id> <query_id> <self.counted_data[client_id][query_id] como str>
+QUERY_ENDED = "QUERY_ENDED"             # <client_id> <query_id>
+COUNTED_DATA_SENT = "COUNTED_DATA_SENT" # <client_id> <query_id>
+
+class LogManager(BasicLogManager):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+
+    def log_counted_data(self, fragment: DataFragment, counted_data: dict) -> None:
+        client_id = fragment.get_client_id()
+        query_id = fragment.get_query_id()
+        df_id = fragment.get_id()
+        id_log = f"{RECEIVED_ID} {client_id} {query_id} {df_id}"
+        counted_log = f"{COUNTED_DATA} {client_id} {query_id} {pickle.dumps(counted_data)}"
+        self._add_logs([id_log, counted_log])
+
+    def log_counted_data_sent(self, fragment: DataFragment) -> None:
+        client_id = fragment.get_client_id()
+        query_id = fragment.get_query_id()
+        df_id = fragment.get_id()
+        id_log = f"{RECEIVED_ID} {client_id} {query_id} {df_id}"
+        sent_log = f"{COUNTED_DATA_SENT} {client_id} {query_id}"
+        self._add_logs([id_log, sent_log])
