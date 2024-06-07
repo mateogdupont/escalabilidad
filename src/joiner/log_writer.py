802053f20871e3023ea4c179b9e5fb3294d10fd2
ElianaHarriet <eharriet@fi.uba.ar>
2024-06-07 19:36:16 -0300

    Refactor log_writer.py to use separate log types for book title and data fragment

--- src/joiner/log_writer.py
+++ src/joiner/log_writer.py
@@ -4,7 +4,8 @@ from utils.structs.book import Book
 from utils.structs.data_fragment import DataFragment
 
 # Books logs
-BOOK = "BOOK"                           # <book title> <book como str>
+BOOK_T = "BOOK_T"                       # <book title>
+BOOK_DF = "BOOK_DF"                     # <datafragment como str>
 RECEIVED_ID = "RECEIVED_ID"             # <client_id> <query_id> <df_id>
 SIDE_TABLE_UPDATE = "SIDE_TABLE_UPDATE" # <client_id> <query_id> <book title>
 SIDE_TABLE_ENDED = "SIDE_TABLE_ENDED"   # <client_id> <query_id>
@@ -22,7 +23,9 @@ class LogWriter(BasicLogWriter):
     def log_book(self, book: Book) -> None:
         book_title = book.get_title()
         book_str = book.to_str()
-        self._add_logs([f"{BOOK} {book_title} {book_str}"])
+        t_log = f"{BOOK_T} {book_title}"
+        df_log = f"{BOOK_DF} {book_str}"
+        self._add_logs([t_log, df_log])
 
     def log_side_table_update(self, fragment: DataFragment) -> None:
         client_id = fragment.get_client_id()
