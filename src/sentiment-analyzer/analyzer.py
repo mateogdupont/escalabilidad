2ecd47911fca42f9af024abd8a805a0d431206a1
Mateo <mateomateogd@gmail.com>
2024-06-11 15:52:32 -0300

    feat: adding communication between medic and analyzer

--- src/sentiment-analyzer/analyzer.py
+++ src/sentiment-analyzer/analyzer.py
@@ -1,10 +1,12 @@
 import signal
 import os
+import socket
 from utils.structs.data_chunk import *
 from utils.mom.mom import *
 from utils.structs.review import *
 from utils.structs.data_fragment import *
 from utils.query_updater import *
+from multiprocessing import Process, Event
 from dotenv import load_dotenv # type: ignore
 import logging as logger
 import sys
@@ -15,6 +17,9 @@ from textblob import TextBlob # type: ignore
 def get_sentiment_score(text: str) -> float:
     return round(TextBlob(text).sentiment.polarity, 5)
 
+load_dotenv()
+NODE_TYPE=os.environ["NODE_TYPE"]
+HARTBEAT_INTERVAL=int(os.environ["HARTBEAT_INTERVAL"])
 CATEGORY_FILTER = "CATEGORY"
 YEAR_FILTER = "YEAR"
 TITLE_FILTER = "TITLE"
@@ -26,11 +31,13 @@ TIMEOUT = 50
 class Analyzer:
     def __init__(self):
         logger.basicConfig(stream=sys.stdout, level=logger.INFO)
-        load_dotenv()
         repr_consumer_queues = os.environ["CONSUMER_QUEUES"]
         consumer_queues = eval(repr_consumer_queues)
         self.work_queue = list(consumer_queues.keys())[0]
         self.mom = MOM(consumer_queues)
+        self.medic_addres = (os.environ["MEDIC_IP"], int(os.environ["MEDIC_PORT"]))
+        self.id= os.environ["ID"]
+        self.event = None
         self.results = {}
         self.received_ids = {}
         self.exit = False
@@ -39,6 +46,9 @@ class Analyzer:
     
     def sigterm_handler(self, signal,frame):
         self.exit = True
+        self.mom.close()
+        if self.event:
+            self.event.set()
     
     def save_id(self, data_fragment: DataFragment) -> bool:
         client_id = data_fragment.get_client_id() # TODO: review with feat-multiclient branch
@@ -56,8 +66,8 @@ class Analyzer:
         self.received_ids[client_id][query_id][id] = data_fragment.to_human_readable()
         return True
 
-    def add_and_try_to_send_chunk(self, fragment: DataFragment, node: str):
-        if self.exit:
+    def add_and_try_to_send_chunk(self, fragment: DataFragment, node: str, event):
+        if event.is_set():
             return
         if not node in self.results.keys():
             self.results[node] = []
@@ -66,9 +76,9 @@ class Analyzer:
             data_chunk = DataChunk(self.results[node])
             self.mom.publish(data_chunk, node)
             self.results[node] = []
-    
-    def run(self):
-        while not self.exit:
+
+    def run_analizer(self, event):
+        while not event.is_set():
             msg = self.mom.consume(self.work_queue)
             if not msg:
                 continue
@@ -77,16 +87,28 @@ class Analyzer:
             for data_fragment in data_chunk.get_fragments():
                 if not self.save_id(data_fragment):
                     continue
-                if (not data_fragment.is_last()) and (not self.exit):
+                if (not data_fragment.is_last()) and (not event.is_set()):
                     review_text = data_fragment.get_review().get_text()
                     sentiment_score = get_sentiment_score(review_text)
                     query_info = data_fragment.get_query_info()
                     query_info.set_sentiment(sentiment_score)
                     data_fragment.set_query_info(query_info)
                 for fragment, key in update_data_fragment_step(data_fragment).items():
-                    self.add_and_try_to_send_chunk(fragment, key)
+                    self.add_and_try_to_send_chunk(fragment, key, event)
             
             self.mom.ack(tag)
+
+    def run(self):
+        self.event = Event()
+        analyzer_proccess = Process(target=self.run_analizer, args=(self.event,))
+        analyzer_proccess.start()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while not self.exit and analyzer_proccess.is_alive():
+            msg = NODE_TYPE + "." + self.id + "$"
+            logger.info(f"Voy a mandar {msg} a { self.medic_addres}")
+            sock.sendto(msg.encode(), self.medic_addres)
+            time.sleep(HARTBEAT_INTERVAL)
+        analyzer_proccess.join()
 
 def main() -> None:
     analyzer = Analyzer()
