d4f0d8f0de50a8384be19de79d49168691ae41b3
Mateo <mateomateogd@gmail.com>
2024-06-25 21:11:01 -0300

    Sentiment-Analyzer node send hartbeat to all medics

--- src/sentiment_analyzer/analyzer.py
+++ src/sentiment_analyzer/analyzer.py
@@ -22,6 +22,8 @@ def get_sentiment_score(text: str) -> float:
 load_dotenv()
 NODE_TYPE=os.environ["NODE_TYPE"]
 HARTBEAT_INTERVAL=int(os.environ["HARTBEAT_INTERVAL"])
+MEDIC_IP_ADDRESSES=eval(os.environ.get("MEDIC_IPS"))
+MEDIC_PORT=int(os.environ["MEDIC_PORT"])
 CATEGORY_FILTER = "CATEGORY"
 YEAR_FILTER = "YEAR"
 TITLE_FILTER = "TITLE"
@@ -41,7 +43,6 @@ class Analyzer:
         self.mom = MOM(consumer_queues)
         self.results = log_recoverer.get_results()
         self.received_ids = log_recoverer.get_received_ids()
-        self.medic_addres = (os.environ["MEDIC_IP"], int(os.environ["MEDIC_PORT"]))
         self.id= os.environ["ID"]
         self.event = None
         self.exit = False
@@ -124,7 +125,10 @@ class Analyzer:
         while not self.exit and analyzer_proccess.is_alive():
             msg = NODE_TYPE + "." + self.id + "$"
             try:
-                sock.sendto(msg.encode(), self.medic_addres)
+                for id,address in MEDIC_IP_ADDRESSES.items():
+                    complete_addres = (address, MEDIC_PORT)
+                    sock.sendto(msg.encode(), complete_addres)
+                    logger.error(f"Hartbeat sent to medic with id: {id}")
             except Exception as e:
                 logger.error(f"Error sending hartbeat: {e}")
             finally:
